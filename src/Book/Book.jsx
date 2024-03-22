15eb5876e50a549ed67c7d3270c3ad75f4f7a71f
Jay <jdragon2050@gmail.com>
2024-03-22 10:31:33 -0700

    add useeffect for checking if books have been added

--- src/Book/Book.jsx
+++ src/Book/Book.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 
 const Book = ({book}) => {
     const {title,author, description} = book;
     const [showReadMore, setShowReadMore] = useState(false);
+    const [booksArray, setBooksArray] = useState([]);
     const handleClick = () => {
         setShowReadMore((prev) => !prev);
     };
+
+    // const handleAdd = (e) => {
+
+    //    const bookTitle = e.target.parentElement.childNodes[1].innerText
+    //    const books = JSON.parse(localStorage.getItem('books') || '[]');
+    //    console.log(typeof books);
+    //    if(!books.includes(bookTitle)){
+    //        books.push(bookTitle);
+    //    }
+    //    setBooksArray(books);
+    
+    //    if(!books.includes(bookTitle)){
+    //      localStorage.setItem('books', JSON.stringify([...booksArray]));
+    //    }else{
+    //     alert('book has already been added')
+    //    }
+  
+    //    alert(`${bookTitle} has been added`);
+    // }
+
+    const handleAdd = (e) => {
+      const bookTitle = e.target.parentElement.childNodes[1].innerText;
+      const books = JSON.parse(localStorage.getItem('books') || '[]');
+
+      if (!books.includes(bookTitle)) {
+        const updatedBooks = [...books, bookTitle];
+        setBooksArray(updatedBooks);
+        localStorage.setItem('books', JSON.stringify(updatedBooks));
+        alert(`${bookTitle} has been added`);
+      } else {
+        alert('Book has already been added');
+      }
+    };
+
+
   return (
     <div
       style={{
@@ -21,8 +57,12 @@ const Book = ({book}) => {
         maxWidth: '800px',
       }}
     >
+        <button onClick={handleAdd} style={{ backgroundColor:'gray', color:'white', }}>Add</button>
+
+      
       <h2 style={{ color: 'gray' }}>{title}</h2>
       <h3>{author}</h3>
+      
       {showReadMore ? (
         <div
           style={{
--- src/Books/Books.jsx
+++ src/Books/Books.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { programmingBooks as BooksList } from '../BooksList'
 import Book from '../Book/Book'
 const Books = () => {
-    const [showReadMore, setShowReadMore] = useState(false);
 
- 
+    useEffect(() => {
+        const books = localStorage.getItem('books');
+        if(books){
+            console.log("check out more books");
+        }else{
+            console.log("should add more books");
+        }
+    }, [])
+
+    const handleClick = () => {
+        const books = localStorage.getItem('books');
+        if(books){
+            alert(books)
+        }else(
+            alert('click the Add button to add to favorites')
+        )
+    }
 
   return (
     <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', minHeight:'100vh',}}>
@@ -15,6 +30,7 @@ const Books = () => {
                 <Link to='/'>Home</Link>
             </li>
         </div>
+        <button style={{marginTop:'2rem'}} onClick={handleClick}>List of Favorite Books</button>
         <h2>List of helpful books:</h2>
         {BooksList.map((book,index) => {
             return (
